d44c5e26bd43d6465fa2fc0524c828cdb1229ca8
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-04-27 20:03:00 +0200

    init envfile & * route

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
+
+const {
+  APP_PORT,
+  APP_HOSTNAME
+} = process.env;
+
+const {
+  version
+} = require('./package.json');
+
 const express = require('express');
 
 const app = express();
 
-app.listen(4000, () => {
+app.get('*', (req, res) => {
+  res.json({
+    version
+  });
+});
+
+app.listen(APP_PORT, APP_HOSTNAME, () => {
   console.log(`[expess] listening at port 4000`);
 });
